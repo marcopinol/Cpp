11ceb96c4e75a689c7da95f625d5df0ba8a51d00
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2024-11-26 12:50:44 -0300

    xmas message using nested fors

---
+++ xmas.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	system("cls");
+	
+	cout << "Sinal interno direito: ";
+	char sd = getche();
+	
+	cout << "\nSinal interno esquerdo: ";
+	char se = getche();
+	cout << "\n\n";
+	
+	for(int i = 0; i < 4; i++){
+		for(int k = 1; k < 5; k++){
+			for(int j = 1; j <= 40 - (2 * i + k); j++) cout << ' ';
+				cout << '/';
+			for(int j = 1; j < (2 * i + k); j++) cout << se;
+				for(int j = 1; j < (2 * i + k); j++) cout << sd;
+					cout << '\\' << endl;	
+		}	
+	}
+	
+	for( int i = 0; i < 2; i++){
+		for(int j = 0; j < 38; j++){
+			cout << ' ';
+		}
+		cout << "|  |" << endl;
+	}
+	cout << endl;
+	
+	for(int i = 0; i < 34; i++){
+		cout << ' ';
+	}
+	cout << "FELIZ NATAL!" << endl;
+	for(int i = 0; i < 30; i++){
+		cout << ' ';
+	}
+	cout << "E UM PROSPERO 2025!" << endl;
+	
+	return 0;
+}
