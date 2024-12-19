baac21b28157a12ee047f9d4325e9547220e5e15
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2024-12-19 14:39:41 -0300

    Macros

---
+++ macro-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define SOMA(x, y) (x + y)
+using namespace std;
+
+int main(){
+	int z;
+	
+	z = 10 * SOMA(3, 4);
+	
+	cout << z << endl;
+	
+	system("PAUSE");
+	
+	return 0;
+}
---
+++ prnmacro.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define PRN(n) cout << n << endl
+using namespace std;
+
+int main(){
+	int n1 = 1416, n2 = n1 / 2;
+	
+	PRN(n1);
+	PRN(n2);
+	
+	system("PAUSE");
+	
+	return 0;
+}
