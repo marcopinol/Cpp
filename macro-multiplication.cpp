c84ff8e18de0002ea7d7ac0962a792a81cb22fd0
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2024-12-19 14:43:11 -0300

    More macros

---
+++ macro-multiplication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define PROD(x, y) ((x) * (y))
+using namespace std;
+
+int main(){
+	int z;
+	
+	z = PROD(2 + 3, 5);
+	
+	cout << z << endl;
+	
+	system("PAUSE");
+	
+	return 0;
+}
