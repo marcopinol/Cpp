f744cf6159efd9187e037270b6205a6ae11d599d
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2024-12-27 00:39:35 -0300

    Adding Inverter functionality
    Adding Inverter functionality and with it fixing some bugs in the application

---
+++ practice/roulette/roulette-v1.1.2.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+
+using namespace std;
+
+void percentages(const float& f, const float& t);
+void showInfo(const int& t, const int& f, const bool& b);
+int invertert(int& f, int& t, bool& b);
+int inverterf(int& t, int& f, bool& b);
+
+int main(){
+	char ch;
+	bool b = false;
+	do{
+		system("cls");
+		int t, f;
+		
+		cout << "\n\t+-+-+-+-+ WELCOME TO ROULETTE BULLET COUNTER v1.1.2 +-+-+-+-+\n";
+		cout << "\t ,________________________________       \n";
+		cout << "\t|__________,----------._ [____]  \"\"-,__  __...-----===\n";
+		cout << "\t        (_(||||||||||||)___________/   ""               |\n";
+		cout << "\t           `----------'        [ ))\"-,                |\n";
+		cout << "\t                                \"\"    `,  _,--...___  |\n";
+		cout << "\t                                        `/          \"\"\"\n";
+		cout<< "\t\t\t\t\tprogram by lonipocram\n\n";
+		cout << "\n\tInsert the number of true bullets: ";
+		cin >> t;
+		cout << "\n\n\tInsert the number of false bullets: ";
+		cin >> f;
+		
+		system("cls");
+					
+		showInfo(t, f, b);
+		percentages(f, t);
+		
+		while((t + f) != 0){
+			char c;
+			
+			cout << "\n\n\n\tPress \"T\" to eject a true bullet or \"F\" to eject a false bullet";
+			
+			c = getch(); 
+			
+			if(c == 73 || c == 105){
+				b = !(b);
+				showInfo(t, f, b);
+				percentages(f, t);
+			}
+			
+			if(c == 84 || c == 116){
+				if(t <= 0){
+					system("cls");
+					
+					if(t == 0 && f == 0){
+						showInfo(t, f, b);
+						break;
+					}	
+					
+					if(b == true){
+						invertert(f, t, b);
+						showInfo(t, f, b);
+						percentages(f, t);
+						continue;
+					}
+					
+					cout << "\n\n\t\tTrue bullets gone";
+					showInfo(t, f, b);
+					percentages(f, t);
+				}else{
+					system("cls");
+					
+					if(t == 0 && f == 0){
+						showInfo(t, f, b);
+						break;
+					}
+					
+					if(b == true){
+						invertert(f, t, b);	
+						showInfo(t, f, b);
+						percentages(f, t);		
+					}else{
+						t -= 1;
+						showInfo(t, f, b);
+						percentages(f, t);
+					}
+				}
+			}else if(c == 70 || c == 102){
+				if(f <= 0){
+					system("cls");
+					
+					if(t == 0 && f == 0){
+						showInfo(t, f, b);
+						break;
+					}	
+					
+					if(b == true){
+						inverterf(t, f, b);
+						showInfo(t, f, b);
+						percentages(f, t);
+						continue;
+					}
+					
+					cout << "\n\n\t\tFalse bullets gone";
+					showInfo(t, f, b);
+					percentages(f, t);
+				}else{
+					system("cls");
+					
+					if(t == 0 && f == 0){
+						showInfo(t, f, b);
+						break;
+					}	
+					
+					if(b == true){						
+						inverterf(t, f, b);
+						showInfo(t, f, b);
+						percentages(f, t);
+					}else{					
+						f -= 1;
+						showInfo(t, f, b);
+						percentages(f, t);
+					}
+				}
+			}else{
+				system("cls");
+				showInfo(t, f, b);
+				percentages(f, t);
+			}
+		}
+	system("cls");
+	
+	showInfo(t, f, b);
+	cout << "\n\n\tDo you want to replay? (y/n)";
+	
+	ch = getch();
+	}while(ch == 89 || ch == 121);
+	
+	cout << endl;
+	
+	return 0;
+}
+
+void percentages(const float& f, const float& t){
+	float pf, pt;
+	
+	pf = ((float)(f) / (float)(t + f)) * 100;
+	pt = ((float)(t) / (float)(t + f)) * 100;
+	
+	cout << setiosflags(ios::showpoint) << setiosflags(ios::fixed) << setprecision(2);
+	cout << "\n\n\tChance to being true: " << pt << '%';
+	cout << "\n\tChance to being false: " << pf << '%';
+}
+
+void showInfo(const int& t, const int& f, const bool& b){
+	cout << "\n\n\t\tTotal of bullets: " << (t + f);
+	cout << "\n\n\tTrue bullets: " << t;
+	cout << "\n\tFalse bullets: " << f;
+	b == true ? cout << "\n\tINVERTER: ON" : cout << "\n\tINVERTER: OFF";
+}
+
+int invertert(int& f, int& t, bool& b){
+	if(b == true && f == 0){
+		cout << "\n\n\t\tNot possible";
+		b = false;
+		return f;
+	}else if(b == true){
+		b = false;
+		return f--;
+	}else
+		return t--;
+}
+
+int inverterf(int& t, int& f, bool& b){
+	if(b == true && t == 0){
+		cout << "\n\n\t\tNot possible";
+		b = false;
+		return t;
+	}else if(b == true){
+		b = false;
+		return t--;
+	}else
+		return f--;
+}
