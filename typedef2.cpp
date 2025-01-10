a24a166e23eb26f4c5ddc0189953a82d880480c0
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2025-01-10 17:06:38 -0300

    Update typedef2.cpp

--- typedef2.cpp
+++ typedef2.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
-typedef void FuncType(int x, float y); //study more in depth late
+typedef void FuncType(char x, float y); //study more in depth late
 FuncType minhaFunc;
 
 int main(){
+	minhaFunc(97, 14.5);
+	cout << endl;
 	
 	system("PAUSE");
 	return 0;
+}
+
+void minhaFunc(char x, float y){
+	cout << "y: " << y << " x : " << x;
 }
