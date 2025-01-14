87988f7546ac64e76957dc1ead52664ba72d9eba
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2025-01-14 14:59:35 -0300

    Destructors

---
+++ module 2/Cap 8 - Class and Objects/destructors.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Rec{
+	private:
+		static int n; //an unique item for all objects
+	public:
+		Rec(){ //Default constructor
+			n++;
+		}
+		int getRec() const{
+			return n;
+		}
+		~Rec(){ //Destructor
+			n--;
+		}
+};
+
+int Rec::n = 0; //Need for compile time
+
+int main(){
+	Rec r1, r2, r3; //Executes the default constructor 3 times
+	
+	cout << r1.getRec() << endl;
+	cout << r2.getRec() << endl;
+	cout << r3.getRec() << endl;
+	
+	{
+		Rec r4, r5, r6;
+		cout << r1.getRec() << endl;
+	}
+	
+	cout << r1.getRec() << endl;
+	
+	system("PAUSE");
+	return 0;
+}
