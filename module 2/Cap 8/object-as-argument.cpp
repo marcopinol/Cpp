bd94c43d9d3f7666e32c1dff1b834e9663b89e4b
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2025-01-14 13:50:52 -0300

    Object as argument

---
+++ module 2/Cap 8/object-as-argument.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class Venda{
+	private:
+		int npecas;
+		float preco;
+	public:
+		Venda(): npecas(0), preco(0.0) //Default constructor
+		{}
+		Venda(int np, float p){ //Constructor with arguments
+			npecas = np;
+			preco = p;
+		}
+		void getVenda();
+		void printVenda();
+		void addVenda(Venda v1, Venda v2);
+};
+
+int main(){
+	Venda A(58, 12734.53), B, Total;
+	
+	B.getVenda();
+	Total.addVenda(A, B);
+	cout << endl;
+	
+	cout << "Venda A..........";
+	A.printVenda();
+	cout << "Venda B..........";
+	B.printVenda();
+	cout << "Totais...........";
+	Total.printVenda();	
+	
+	system("PAUSE");
+	return 0;
+}
+
+void Venda::getVenda(){
+	cout << "Insira o numero de pecas: ";
+	cin >> npecas;
+	cout << "Insira o valor: ";
+	cin >> preco;
+}
+
+void Venda::printVenda(){
+	cout << setiosflags(ios::fixed) 
+		 << setiosflags(ios::showpoint) << setprecision(2) << setw(10) 
+		 << npecas << setw(10) << preco << endl;
+	cout << setw(10) << preco << endl;	
+}
+
+void Venda::addVenda(Venda v1, Venda v2){
+	npecas = v1.npecas + v2.npecas;
+	preco = v1.preco + v2.preco;
+}
