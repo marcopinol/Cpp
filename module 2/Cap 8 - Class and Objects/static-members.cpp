bfabcc086ebdbe49eba34b81ca5ecaa7510d0108
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2025-01-14 14:43:13 -0300

    Static members

---
+++ module 2/Cap 8 - Class and Objects/static-members.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Rec{
+	private:
+		static int n;
+	public:
+		Rec(){ //Default constructor
+			n++;
+		}
+		int getRec() const{
+			return n;
+		}
+};
+
+int Rec::n = 0; //Need for compile time
+
+int main(){
+	Rec r1, r2, r3; //Executes the default constructor 3 times
+	
+	cout << r1.getRec() << endl;
+	cout << r2.getRec() << endl;
+	cout << r3.getRec() << endl;
+	
+	system("PAUSE");
+	return 0;
+}
