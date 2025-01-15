cdba6cabe10908f1e77d4522260710bf0243eb4f
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2025-01-15 14:37:27 -0300

    Object array initialization

---
+++ module 2/Cap 8 - Class and Objects/object-array2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+
+#define MAX 100
+using namespace std;
+
+class Venda{
+	private:
+		int npecas;
+		float preco;
+	public:
+		Venda(): npecas(0), preco(0.0) //Default constructor
+		{}
+		Venda(int np, float p){ //Constructor with arguments
+			npecas = np;
+			preco = p;
+		}
+		void getVenda();
+		void printVenda();
+		Venda addVenda(Venda v1);
+};
+
+int main(){
+	Venda A[MAX] = { Venda(50, 123.5), Venda(), Venda(15, 38.4)}; //object array initialization
+	Venda Total;
+	
+	int i = 3;
+	char resp;
+	
+	do{
+		A[i++].getVenda();
+		cout << "Gostaria de adicionar mais uma venda?";
+		cin >> resp;
+	}while(resp != 'n' && i < MAX);
+	
+	for(int j = 0; j < i; j++){
+		cout << "Venda ............";
+		A[j].printVenda();
+		Total = A[j].addVenda(Total);
+	}
+	cout << "Total .............";
+	Total.printVenda();
+	cout << endl;
+	
+	system("PAUSE");
+	return 0;
+}
+
+void Venda::getVenda(){
+	cout << "Insira o numero de pecas: ";
+	cin >> npecas;
+	cout << "Insira o valor: ";
+	cin >> preco;
+}
+
+void Venda::printVenda(){
+	cout << setiosflags(ios::fixed) 
+		 << setiosflags(ios::showpoint) << setprecision(2) << setw(10) 
+		 << npecas << setw(10) << preco << endl;
+	cout << setw(10) << preco << endl;	
+}
+
+Venda Venda::addVenda(Venda v){
+	Venda temp;
+	temp.npecas = npecas + v.npecas;
+	temp.preco = preco + v.preco;
+	
+	return temp;
+}
