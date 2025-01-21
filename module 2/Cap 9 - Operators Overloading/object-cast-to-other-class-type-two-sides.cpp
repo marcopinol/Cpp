4c69a701aa68561720a2da53922b2a441bfe556b
Marco Antonio <91616590+marcopinol@users.noreply.github.com>
2025-01-20 21:15:54 -0300

    Making both conversions

---
+++ module 2/Cap 9 - Operators Overloading/object-cast-to-other-class-type-two-sides.cpp
@@ -0,0 +1,66 @@
+//Making both conversions
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const double PI = 3.141592653;
+
+class Radianos{
+	private:
+		double rad;
+	public:
+		Radianos(): rad(0.0) //Default constructor
+		{}
+		
+		Radianos(double r): rad(r) //One arg constructor
+		{}
+		
+		operator double(){ //double type converter
+			return rad;
+		}		
+		
+		void printR(){
+			cout << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setprecision(2) << rad << " rad" << endl;
+		}
+};
+
+class Graus{
+	private:
+		double g;
+	public:
+		Graus(): g(0.0) //Default constructor
+		{}
+		
+		Graus(double x): g(x) //One arg constructor
+		{}
+		
+		Graus(Radianos R); //Constructor that casts Radianos to Graus
+		operator Radianos() const; //Casting function Graus to Radianos
+		
+		void printG(){
+			cout << setiosflags(ios::fixed) << setiosflags(ios::showpoint) << setprecision(2) << g << "\xF8" << endl;
+		}
+};
+
+Graus::Graus(Radianos R){ //Radianos to Graus
+	g = (static_cast<double>(R) * 180.0) / PI;
+}
+
+Graus::operator Radianos() const{ //Graus to Radianos
+	return Radianos(g * PI / 180.0);
+}
+
+int main(){
+	Graus gr, gA(180.0);
+	Radianos rad(PI), rA;
+	
+	gr = rad; //Casts Radianos to Graus
+	rA = gA; //rA = gA.operator Radianos();
+	
+	gr.printG();
+	rA.printR();
+	cout << endl;
+	
+	system("PAUSE");
+	return 0;
+}
