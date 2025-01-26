0db0e14b2c85aa06fea340741152c2274c391601
marcopinol <voidr0id@tuta.io>
2025-01-26 16:17:20 -0300

    Showing variables memory address

---
+++ module 2/Cap 11 - Pointers/address-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int i, j, k;
+	
+	cout << "Endereco de i = " << &i << endl; //& operator shows the memory address of his operand variable, in this case, i;
+	cout << "Endereco de j = " << &j << endl;
+	cout << "Endereco de k = " << &k << endl;
+	
+	system("PAUSE");
+	return 0;
+}
